b14a65c0829872e5cccde9f1b5b5d167e20a48fa
hudamin <hdm258i@gmail.com>
2017-06-26 16:51:41 +0800

    update

--- data/code-with-eval.js
+++ data/code-with-eval.js
@@ -1,9 +1,52 @@
+// comments
+'use catch';
+eval( 'var g = 123; console.log( g ); ' );
 ( function() {
     eval( 'var a = 345; console.log( 1/a );' );
+
+    var aa = eval( '12345;' ) || 1; 
 
     function abc() {
         console.log( 'abc' );
     }
 
+    function Person( name ) {
+        this.name = name;
+    }
+
+    Person.prototype.sayHello = function() {
+        console.log( 'Hello' );
+    };
+
+    function outer() {
+        return _.every( function( o ) { return 'string' == typeof o; } );
+    }
+
+    var a = function() {
+        console.log( 'a' );
+    };
+
+    var obj = {
+            a: 123
+            , b: function() {
+                console.log( b );
+            }
+            , c: eval( '12345' )
+        };
+
+    const key = Symbol( 123 );
+
+    obj[ key ] = function() {
+        console.log( 123 );
+    };
+
+    var c = 1 == 0 ? 0 : function() { console.log( 'c' ); };
+
     abc();
 } )();
+
+var m = function() {
+    console.log( 'm' );
+};
+
+eval( 'var g = 123; console.log( g ); ' );
--- src/wrap-eval.js
+++ src/wrap-eval.js
@@ -6,6 +6,14 @@ var codeFile = path.resolve( dataRoot, 'code-with-eval.js' );
 // var codeFile = path.resolve( dataRoot, 'voice-baidu.js' );
 var codeFileTry = path.resolve( dataRoot, 'try-catch.js' );
 
+//             var ast = UglifyJS.parse( 
+//                     // '( function() { try {} catch( e ) { throw Error(e); } } )()' 
+//                     'try {} catch( e ) { throw Error(e); }' 
+//                 ); 
+// 
+// console.log( JSON.stringify( ast ) );
+// return;
+
 var deep_clone = new UglifyJS.TreeTransformer( function( node, descend ) {
         node = node.clone();
         descend( node, this );
@@ -19,23 +27,111 @@ var ast = UglifyJS.parse( code, { filename: codeFile } );
 var astTry = UglifyJS.parse( codeTry );
 
 // console.log( JSON.stringify( ast ) );
-// ast.figure_out_scope();
+ast.figure_out_scope();
+
+function getNodeDesc( node, maxlen ) {
+    if ( 'string' == typeof node || 'number' == typeof node ) {
+        return node;
+    }
+
+    var _stream = UglifyJS.OutputStream( {
+            beautify: true
+        } );
+    node.print( _stream );
+    var _outputCode = _stream.toString();
+    return _outputCode.substr( 0, maxlen );
+}
 
 var wrap_eval = new UglifyJS.TreeTransformer( function( node, descend ) {
+        var needCatch = this.has_directive( 'use catch' );
         // console.log( node.start.file, node.start.type );
-        if ( node instanceof UglifyJS.AST_Call && node.expression.name == 'eval' ) {
-            // console.log( '==============================' );
-            // console.log( node );
-            var nodeTry = astTry.transform( deep_clone );
+        if ( node instanceof UglifyJS.AST_Call 
+            && node.expression.name == 'eval' 
+            && needCatch ) {
+            var parentNode = this.parent( 1 );
+            console.log( 'eval ======');
+            if ( 
+                parentNode instanceof UglifyJS.AST_Toplevel
+                || ( parentNode instanceof UglifyJS.AST_Defun
+                    || parentNode instanceof UglifyJS.AST_Function 
+                ) ) {
+                console.log( 'below function eval' );
+            // console.log( parentNode ); 
+            // var nodeTry = astTry.transform( deep_clone );
+            var nodeTry = UglifyJS.parse( 
+                    'try {} catch( e ) { throw Error(e); }' 
+                    // '( function() { try {} catch( e ) { throw Error(e); } } )()' 
+                    // '( ( function() { try {} catch( e ) { throw Error(e); } } )() || 0 )' 
+                ); 
+            // var _node = new UglifyJS.AST_Block();
+            // _node.body.push( new UglifyJS.AST_Function() );
+            // _node.body[ 0 ].body.expression.push( new UglifyJS.AST_Try() );
+            // _node.body[ 0 ].body.expression.body[ 0 ].bcatch = new UglifyJS.AST_Catch();
+            // _node.body[ 0 ].body.expression.body[ 0 ].bcatch.argname = 'e';
+            // _node.body[ 0 ].body.expression.body[ 0 ].bcatch.body.push( new UglifyJS.AST_Throw() );
+            // _node.body[ 0 ].body.expression.body[ 0 ].bcatch.body[ 0 ].value
+            //     =  new UglifyJS.AST_Call() ;
+            // _node.body[ 0 ].body.expression.body[ 0 ].bcatch.body[ 0 ].value.args.push( 
+                //
             nodeTry.body[ 0 ].body.unshift( node );
+            // nodeTry.body[ 0 ].body.expression.body[ 0 ].body.unshift( node );
+            // _node.body[ 0 ].body.expression.body[ 0 ].body.unshift( node );
+
+            // nodeTry.body[ 0 ].body.left.expression.body[ 0 ].body.unshift( node );
             descend( node, this ); 
             return nodeTry;
+            // return _node;
+            }
         }
 
-        if ( node instanceof UglifyJS.AST_Defun 
-            || node instanceof UglifyJS.AST_Function ) {
+        if ( 
+            ( node instanceof UglifyJS.AST_Defun 
+                || node instanceof UglifyJS.AST_Function 
+            )
+            && needCatch ) {
+
+            var funcName = node.name && node.name.name;
+            if ( funcName ) {
+                console.log( 'name: ' + funcName );
+            }
+            else {
+                // var parentNode = this.find_parent( UglifyJS.AST_Assign );
+                var parentNode = this.parent();
+
+                if ( parentNode instanceof UglifyJS.AST_Assign ) {
+                    // funcName = parentNode.left.end.value;
+                    funcName = getNodeDesc( parentNode.left, 50 );
+                    console.log( '==== assign: ' + funcName + ' ====' );
+                }
+                else if ( parentNode instanceof UglifyJS.AST_VarDef ) {
+                    // funcName = parentNode.name.end.value;
+                    funcName = getNodeDesc( parentNode.name, 50 );
+                    console.log( '==== var: ' + funcName + ' ====' );
+                }
+                else if ( parentNode instanceof UglifyJS.AST_ObjectProperty ) {
+                    // funcName = parentNode.key;
+                    funcName = getNodeDesc( parentNode.key, 50 );
+                    console.log( '==== property: ' + funcName + ' ====' );
+                }
+                else {
+                    funcName = getNodeDesc( node, 50 );
+                    console.log( '==== other: ' + funcName  + ' ====' );
+                }
+            }
             descend( node, this ); 
-            var nodeTry = astTry.transform( deep_clone );
+            if ( node.body.length == 1 
+                && node.body[ 0 ].body
+                && node.body[ 0 ].body.start.value == 'try' ) {
+                return node;
+            }
+            // var nodeTry = astTry.transform( deep_clone );
+            var nodeTry = UglifyJS.parse( 
+                    'try{}catch( e ) { e.message += "\\n[ func error: ' 
+                        + funcName.replace(/["\\]/g, '\\$&')
+                            .replace( /[\r?\n]/g, ' ')
+                            .replace( /\s{2,}/g, '' ) 
+                        + ' ... ]"; throw Error( e ); }' 
+                );
             var oldBody = node.body;
             node.body = [ nodeTry ];
             nodeTry.body[ 0 ].body = nodeTry.body[ 0 ].body.concat( oldBody );
